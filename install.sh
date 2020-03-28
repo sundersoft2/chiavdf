726a84a25067b2662b05dbb98d76d54b0aa26258
Gene Hoffman <hoffmang@hoffmang.com>
2020-03-27 21:44:31 -0700

    pass correct cbuild target

--- install.sh
+++ install.sh
@@ -3,15 +3,13 @@
 # Stop in case of error
 set -e
 
-echo "Hello world"
-
 python -m pip install cibuildwheel==1.3.0
 python -m pip install pep517
 
 # build just python 3.7
-CIBW_BUILD="cp37-*"
+CIBW_BUILD="cp37-win_amd64"
 # don't build i686 targets, can't seem to find cmake for these
-CIBW_SKIP="'*-manylinux_i686'"
+#CIBW_SKIP="'*-manylinux_i686'"
 # we need boost
 #CIBW_MANYLINUX_X86_64_IMAGE: manylinux2014
 #CIBW_BEFORE_BUILD_LINUX: curl -L https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-`uname -m`.sh > cmake.sh && yes | sh cmake.sh | cat && rm -f /usr/bin/cmake && yum -y install boost-devel gmp-devel && python -m pip install --upgrade pip && which cmake && cmake --version
